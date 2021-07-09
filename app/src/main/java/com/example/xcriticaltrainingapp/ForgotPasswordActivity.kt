e3a3781421f388b7f3331b0bcfae09e66e02d9a0
RDesti <tatotschka17@rambler.ru>
2021-07-09 12:04:04 +0300

    added transition beetwen activity, email validation

--- app/src/main/java/com/example/xcriticaltrainingapp/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/xcriticaltrainingapp/ForgotPasswordActivity.kt
@@ -2,10 +2,38 @@ package com.example.xcriticaltrainingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    private val errorEmailFP by lazy { findViewById<TextView>(R.id.textViewErrorEmailFP) }
+    private val textEmailFP by lazy { findViewById<EditText>(R.id.editTextEmailFP) }
+    private val buttonEnterFP by lazy {findViewById<Button>(R.id.buttonSend)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+        initListenersForgPass()
+    }
+
+    private fun initListenersForgPass() {
+        textEmailFP.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                errorEmailFP.visibility = View.INVISIBLE
+            }
+        })
+
+        buttonEnterFP.setOnClickListener {
+            if(textEmailFP.text.isNullOrEmpty()){
+                errorEmailFP.visibility = View.VISIBLE
+            }
+        }
     }
 }
--- app/src/main/java/com/example/xcriticaltrainingapp/MainActivity.kt
+++ app/src/main/java/com/example/xcriticaltrainingapp/MainActivity.kt
@@ -1,25 +1,37 @@
 package com.example.xcriticaltrainingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     private val errorEmail by lazy { findViewById<TextView>(R.id.textViewErrorEmail) }
     private val errorPassword by lazy { findViewById<TextView>(R.id.textViewErrorPassword)}
     private val textEmail by lazy { findViewById<EditText>(R.id.editTextEmail) }
     private val textPassword by lazy { findViewById<EditText>(R.id.editTextPassword)}
+    private val buttonEnter by lazy {findViewById<Button>(R.id.buttonSignIn)}
+    private val textViewForgotPass by lazy { findViewById<TextView>(R.id.textViewForgotPassword) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initListeners()
+
+        textViewForgotPass.setOnClickListener{
+            val transition = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(transition)
+        }
     }
 
-    fun checkEmailText(view: View){
+    private fun initListeners() {
         textEmail.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {}
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -27,9 +39,7 @@ class MainActivity : AppCompatActivity() {
                 errorEmail.visibility = View.INVISIBLE
             }
         })
-    }
 
-    fun checkPasswordText(view: View){
         textPassword.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {}
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -37,17 +47,21 @@ class MainActivity : AppCompatActivity() {
                 errorPassword.visibility = View.INVISIBLE
             }
         })
+
+        buttonEnter.setOnClickListener {
+            if(textEmail.text.isNullOrEmpty() || !(isEmailValid(textEmail.text.toString()))){
+                errorEmail.visibility = View.VISIBLE
+            }
+
+            if(textPassword.text.isNullOrEmpty()){
+                errorPassword.visibility = View.VISIBLE
+            }
+        }
     }
 
-    fun showSignInWindow(view: View){
-        if(textEmail.text.isNullOrEmpty()){
-            errorEmail.visibility = View.VISIBLE
-        }
-
-        if(textPassword.text.isNullOrEmpty()){
-            errorPassword.visibility = View.VISIBLE
-        }
+    private fun isEmailValid(email: String): Boolean {
+        return Pattern.compile(
+            """^[^@\s\.]+@[^@\s]+\.[^@\s]+$"""
+        ).matcher(email).matches()
     }
-
-
 }
