6ba55257a3ac3adec7e1e086a42940bf45b87328
RDesti <tatotschka17@rambler.ru>
2021-07-22 09:04:07 +0300

    open profile with swipe in act_bot_nav

--- app/src/main/java/com/example/xcriticaltrainingapp/BottomNavActivity.kt
+++ app/src/main/java/com/example/xcriticaltrainingapp/BottomNavActivity.kt
@@ -3,16 +3,12 @@ package com.example.xcriticaltrainingapp
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.xcriticaltrainingapp.databinding.ActivityBottomNavBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BottomNavActivity : AppCompatActivity() {
 
@@ -47,7 +43,7 @@ class BottomNavActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.menu -> {
-                drawer.openDrawer(GravityCompat.START)
+
             }
             R.id.grid_four -> {
 
